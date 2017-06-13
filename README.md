7122b0863bc1d4e4f24e57f2dafcf120dd07add0
LenusLtd <h1611919@student.southstaffs.ac.uk>
2017-06-13 15:39:52 +0100

    Updated V3
    Multiple Errors with Github. Had to delete others.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# FlappyBirdsV3
-Updated (Multiple github Errors)
+# FlappyBirdsV2
+For FilthyFrank Video game lovers.
+# flappybirds FilthyFrank TV Game
+ # flappybirds by Jordon Holmes
+  HTML5 and JavaScript Flappy Birds clone using the Phaser.io game engine. 
+ +
+ +view game [here](https://lenusltd.github.io/FlappyBirdsV2/)
---
+++ main.js
@@ -0,0 +1,173 @@
+//Creat our 'main' state that will contain the game
+var mainState = {
+	preload: function() {
+		//This function will be executed at the beginning
+		//That's where we load the images and sound
+		
+		//Load the bird sprite
+		game.load.image('bird', 'assets/frank.png');
+		game.load.image('pipe', 'assets/dubz.png');
+	},
+	
+	create: function() {
+		//This function is called after the preload function
+		//Here we setup the game, display sprites, etc.
+		
+		//Change the background colour of the game to blue - for now!
+		game.stage.backgroundImage = 'assets/dubz.png';
+		
+		//Set the physics for the game
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+		
+		//Display the bird at the position of x=100 and y=245
+		this.bird = game.add.sprite(100, 245, 'bird');
+		
+		//Add the physics to the bird
+		//Needed for: movement, gravity, collisions, etc.
+		game.physics.arcade.enable(this.bird);
+		
+		//Add gravity to the bird to make it fall
+		this.bird.body.gravity.y = 1000;
+        
+        // New anchor position
+
+this.bird.anchor.setTo(-0.2, 0.5);
+		
+		//Call 'jump' function when the spacebar is pressed
+		var spaceBar = game.input.keyboard.addKey(
+						Phaser.Keyboard.SPACEBAR);
+		spaceBar.onDown.add(this.jump, this);
+		
+		//Create an empty group
+		this.pipes = game.add.group();
+		
+		//Timer for pipes
+		this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+        
+        //Score
+
+this.score = 0;
+
+this.labelScore = game.add.text(20, 20, "0",
+
+{ font: "30px Arial", fill: "#ffffff" });
+	},
+	
+	update: function() {
+		//This function is called 60 times per second
+		//It contains the games logic
+		
+		//Call the 'restartGame' function
+		if (this.bird.y <0 || this.bird.y > 490)
+			this.restartGame();
+        
+        // Slowly rotate the bird downward, up to a certain point
+        
+        //calls the restartGame function each time the bird dies
+
+game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame,
+
+null, this);
+
+if (this.bird.angle < 20)
+
+this.bird.angle += 1;
+	},
+	
+	jump: function() {
+		//Add a vertical velocity to the bird
+		this.bird.body.velocity.y = -350;
+        
+        //Simplified code for upward flying animation
+
+game.add.tween(this.bird).to({angle: -20}, 100).start();
+        
+        
+	},
+    
+    hitPipe: function() {
+
+//If the bird has already hit a pipe, do nothing
+
+//It means the bird is already falling off the screen
+
+if (this.bird.alive == false)
+
+return;
+
+//Set the alive property of the bird to false
+
+this.bird.alive = false;
+
+//Prevent new pipes from appearing
+
+game.time.events.remove(this.timer);
+
+//Go through all the pipes, and stop their movement
+
+this.pipes.forEach(function(p){
+
+p.body.velocity.x = 0;
+
+}, this);
+
+},
+	
+	//Restart the game
+	restartGame: function() {
+		//Start the 'main' state, which restarts the game
+	game.state.start('main');
+        
+        //calls the restartGame function each time the bird dies
+
+game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe,
+
+null, this);
+	},
+	
+	//Add a pipe
+	addOnePipe: function(x, y) {
+		//Create a pipe at the position x and y
+		var pipe = game.add.sprite(x, y, 'pipe');
+		
+		//Add pipe to group
+		this.pipes.add(pipe);
+		
+		//Enable the physics on the pipe
+		game.physics.arcade.enable(pipe);
+		
+		//Add velocity to the pipe to make it move left
+		pipe.body.velocity.x = -200;
+		
+		//Automatically kill pipe when it is not longer visible
+		pipe.checkWorldBounds = true;
+		pipe.outOfBoundsKill = true;
+	},
+	
+	//Many pipes
+	addRowOfPipes: function() {
+		//Randomly pick a number between 1 and 5
+		//This will be the hole position in the pipe
+		var hole = Math.floor(Math.random() * 5) + 1;
+		
+		//Add 6 pipes
+		for (var i = 0; i < 8; i++)
+			if (i != hole && i != hole +1)
+				this.addOnePipe(400, i * 60 + 10);
+        
+        //Increases score as new pipes are created
+
+this.score += 1;
+
+this.labelScore.text = this.score;
+	},
+};
+
+//Initialise Phaser, and create a 400px x 490px game
+var game = new Phaser.Game(400, 490);
+
+//Add the 'mainState' and call it 'main'
+game.state.add('main', mainState);
+
+//Start the state to actually start the game
+game.state.start('main');
